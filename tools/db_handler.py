5d0bdad00428b4ef5df7a1e42384417a465bd813
dilo <hesdilo@gmail.com>
2015-05-10 16:19:58 +0800

    mod db_handler

--- tools/db_handler.py
+++ tools/db_handler.py
@@ -5,15 +5,19 @@ Created on 2015年4月23日
 @author: dilo
 '''
 
+import os
 import sqlite3
 from config import DB_PATH, SQL_FILE
 
 db_path     = DB_PATH()
 sql_file    = SQL_FILE()
 
+global g_connect
 
 def init_db():
-    # open database.
+    # create empty database.
+    if os.path.exists(db_path):
+        os.remove(db_path)
     cn = sqlite3.connect(db_path)
     cu = cn.cursor()
     # read and execute SQL file.
@@ -23,6 +27,39 @@ def init_db():
             cu.execute(line)
     # close database.
     cu.close()
+
+
+def open_db():
+    global g_connect
+    if not locals().has_key('g_connect'):
+        g_connect = sqlite3.connect(db_path)
+
+
+def close_db():
+    open_db()
+    g_connect.close()
+
+
+def add_similar(word1, word2, similarity):
+    try:
+        open_db()
+        g_connect.execute('insert into similar values (?, ?, ?)', (word1, word2, similarity))
+        g_connect.commit()
+    except Exception:
+        print word1, word2
+
+
+def get_similar(word, similarity):
+    words = {}
+    open_db()
+    cu = g_connect.cursor()
+    cu.execute('select * from similar where similarity >= %d and word1 = "%s"' % (similarity, word))
+    for i in cu.fetchall():
+        words[i[1]] = i[2]
+    cu.execute('select * from similar where similarity >= %d and word2 = "%s"' % (similarity, word))
+    for i in cu.fetchall():
+        words[i[0]] = i[2]
+    return words
 
 
 if __name__ == '__main__':
--- tools/similar.py
+++ tools/similar.py
@@ -5,5 +5,47 @@ Created on 2015年4月5日
 @author: dilo
 '''
 
+from Levenshtein import distance
+from db_handler import open_db, close_db, add_similar
+
+g_threshold = 70
+
+g_wordsfile = '../vocab/words'
+
+
+def calc_similarity(word1, word2):
+    len1 = len(word1)
+    len2 = len(word2)
+    l = len1
+    if l < len2:
+        l = len2
+    return 100 - (100 / l) * distance(word1, word2)
+
+
+def load_words():
+    words = list()
+    fp = open(g_wordsfile, 'r')
+    for line in fp:
+        words.append(line.rstrip('\n'))
+    fp.close()
+    return words
+
+
+def store_similar():
+    words = load_words()
+    iword = 0
+    nword = len(words)
+    while iword < nword:
+        iiword = iword + 1
+        while iiword < nword:
+            similarity = calc_similarity(words[iword], words[iiword])
+            if similarity >= g_threshold:
+                add_similar(words[iword], words[iiword], similarity)
+            iiword += 1
+        iword += 1
+
+
 if __name__ == '__main__':
-    pass
+    open_db()
+    store_similar()
+    close_db()
